38b84b3aa8346f418f8de8e812eccdc7ed83c192
Simon Beginn <simon@simonmicro.de>
2022-04-12 19:15:40 +0200

    Added CI for dashboard

--- start_config.sh
+++ start_config.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd api
-poetry run python3 main_config.py --config config.sample.yml --redis_host localhost --redis_port 6379 #--debug
+poetry run python3 main_config.py --config config.yml --redis_host localhost --redis_port 6379 #--debug
