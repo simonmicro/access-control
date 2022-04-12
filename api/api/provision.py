9decd61c01b2845525c53b9586e33ec863a210f3
Simon Beginn <simon@simonmicro.de>
2022-04-12 16:49:09 +0200

    Corrected dummy state

--- api/api/provision.py
+++ api/api/provision.py
@@ -8,6 +8,6 @@ def requestProvision(redisClient):
 def getProvisionState(redisClient) -> ProvisionInfo:
     s = redisClient.get('provision/state')
     if s is None:
-        return ProvisionInfo(id=None, state=False, since=None)
+        return ProvisionInfo(id=-1, state=False, since=None)
     s = json.loads(s)
     return ProvisionInfo(id=s['id'], state=s['state'], since=s['since'])
