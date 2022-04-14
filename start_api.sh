57f071a1d32f4d34070bf5e0cd1022c5d3266178
Simon Beginn <simonmicro@outlook.de>
2022-04-14 17:50:37 +0200

    Moved server into "main"

--- start_api.sh
+++ start_api.sh
@@ -6,8 +6,8 @@ export REDIS_HOST=127.0.0.1
 export REDIS_PORT=6379
 
 # For development
-poetry run uvicorn api.server:app --reload
+poetry run uvicorn main:app --reload
 #sleep 4
 
 # For production
-#poetry run uvicorn api.server:app --workers $(nproc) --no-server-header
+#poetry run uvicorn main:app --workers $(nproc) --no-server-header
