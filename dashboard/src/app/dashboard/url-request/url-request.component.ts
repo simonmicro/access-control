bd376b8e07e16a1d2221a120e77131cc474ce17d
Simon Beginn <simon@simonmicro.de>
2022-05-08 21:49:34 +0200

    Implemented URL rewrite

--- dashboard/src/app/dashboard/url-request/url-request.component.ts
+++ dashboard/src/app/dashboard/url-request/url-request.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, Query } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { APIIP, APIScope, APIService } from 'src/app/api.service';
@@ -25,11 +25,11 @@ export class UrlRequestComponent implements OnInit {
   }
 
   private setStatus(id: number, success: boolean) {
-    for(let i = 0; i < id && i < this.statusList.length; ++i) {
+    for (let i = 0; i < id && i < this.statusList.length; ++i) {
       this.statusList[i] = 2;
     }
     this.statusList[id] = success ? 2 : 3;
-    for(let i = id + 1; i < this.statusList.length; ++i) {
+    for (let i = id + 1; i < this.statusList.length; ++i) {
       this.statusList[i] = success ? 0 : 3;
     }
   }
@@ -41,16 +41,16 @@ export class UrlRequestComponent implements OnInit {
       // Determine scope
       try {
         const scopes: APIScope[] = await this.apiSvc.getScopes();
-        for(let scope of scopes) {
-          if(this.data.url.host == scope.url.host)
-          foundScope = scope;
+        for (let scope of scopes) {
+          if (this.data.url.host == scope.url.host)
+            foundScope = scope;
         }
-        if(foundScope == null) {
-          this.snackbar.open('You are missing the permission to access this scope.', '', {duration: 10000});
+        if (foundScope == null) {
+          this.snackbar.open('You are missing the permission to access this scope.', '', { duration: 10000 });
           throw 0;
         }
         this.setStatus(0, true);
-      } catch(e) {
+      } catch (e) {
         throw 0;
       }
 
@@ -59,7 +59,7 @@ export class UrlRequestComponent implements OnInit {
       try {
         myIP = await this.apiSvc.getPublicIP();
         this.setStatus(1, true);
-      } catch(e) {
+      } catch (e) {
         throw 1;
       }
 
@@ -68,44 +68,54 @@ export class UrlRequestComponent implements OnInit {
       try {
         myIPs = await this.apiSvc.getIPs(false);
         this.setStatus(2, true);
-      } catch(e) {
+      } catch (e) {
         throw 2;
       }
 
       // Determine if we still need to add this ip...
       let skipAddIP: boolean = false;
-      for(let ip of myIPs) {
-        if(ip.ip == myIP) {
+      for (let ip of myIPs) {
+        if (ip.ip == myIP) {
           skipAddIP = true;
           break;
         }
       }
 
       try {
-        if(skipAddIP) {
+        let provisionSub = null;
+        if (skipAddIP) {
           // We are already done, so no waiting for the provision here!
         } else {
-          // TODO Register for provision update
+          // Register for provision update
+          provisionSub = new Promise<void>((resolve, reject) => {
+            let sub = this.apiSvc.subscribeToProvision(resp => {
+              if (!resp.state) {
+                resolve();
+                sub.unsubscribe();
+              }
+            });
+            // Fixme... Handle the case the provision hangs or never triggers?
+          });
 
           // Add my ip
           await this.apiSvc.addIP(myIP, 'Direct IP');
-
-          // Now we have to wait for the provision to finish...
-          // TODO
         }
         this.setStatus(3, true);
-      } catch(e) {
+        // Now we have to wait for the provision to finish (if set)...
+        if (provisionSub)
+          await provisionSub;
+      } catch (e) {
         console.error(e);
-        this.snackbar.open('Something went wrong while adding your IP.', '', {duration: 10000});
+        this.snackbar.open('Something went wrong while adding your IP.', '', { duration: 10000 });
         throw 3;
       }
 
       this.setStatus(4, true);
 
       // Now we have to return the user to his original uri
-      // TODO
-      this.setStatus(5, false);
-    } catch(e) {
+      window.location.href = this.data.url.toString();
+      this.setStatus(5, true);
+    } catch (e) {
       // @ts-ignore (TS tries to see a non-number here...)
       this.setStatus(e, false);
     } finally {
